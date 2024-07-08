3642ce2630e862cb9eac775bda86dea645b8a11f
Eternal-G <102162302+Eternal-G@users.noreply.github.com>
2024-07-08 21:49:07 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "EternalChat",
+  description: "你的最佳AI助手",
   appleWebApp: {
-    title: "NextChat",
+    title: "EternalChat",
     statusBarStyle: "default",
   },
 };
